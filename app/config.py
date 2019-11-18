ab3da86a533531f8c67af26a6f769d81138a3995
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-18 11:04:45 +0300

    add movie_test.py file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://api.thenewsdb.org/3/news/{}?api_key={}'
     pass
 
 
