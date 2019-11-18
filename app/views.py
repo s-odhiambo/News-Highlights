14e3f76e854f131a2f8c7879194185e91e0841d0
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-18 09:58:28 +0300

    add news view page function that retiurns the news details page

--- app/views.py
+++ app/views.py
@@ -4,4 +4,9 @@ from app import app
 @app.route('/')
 def index():
   
-  return render_template('index.html')
+  return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def news(news_id)
+
+return render_template('news.html',id = news_id)
