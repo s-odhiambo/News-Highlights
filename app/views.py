e19cc157da6cea27e5d206c29d8c858b922831b8
s-odhiambo <samuelangienda1998@gmail.com>
2019-11-18 12:50:42 +0300

    add news categories in index.html

--- app/views.py
+++ app/views.py
@@ -37,6 +37,13 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
+  
+  popular_news = get_news('popular')
+  print(popular_news)
+  
+  title = 'Home - Welcome to The best News Reviews Website Online'
+  
+  return render_template('index.html', title = title,popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
 
 
 
